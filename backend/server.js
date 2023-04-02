259b489b1ad9a39a3a6ee6839aa4338a4689c353
shajib126 <shajib126@gmail.com>
2023-04-03 01:01:00 +0600

    done

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+const {PrismaClient} = require('@prisma/client')
+const app = express()
+
+const prisma = new PrismaClient()
+
+dotenv.config()
+
+app.use(express.json({limit:"50mb"}))
+app.use(cookieParser())
+
+async function main(){
+    await prisma.$connect()
+}
+main().then(async ()=>{
+    await prisma.$disconnect()
+}).catch(async(e)=>{
+    console.log(e);
+    await prisma.$disconnect()
+    process.exit(1)
+})
+const port = process.env.PORT
+app.listen(port,()=>console.log(`server running in localhost/${port}`))
